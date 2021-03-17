5d090e958eb02ea8dba096c937879c7a8243a535
Vinicius Perillo <vinicius.perillo25@gmail.com>
2021-03-17 11:41:53 -0300

    Reorganização do figure.js

--- scripts/classes/figure.js
+++ scripts/classes/figure.js
@@ -1,8 +1,17 @@
 /** 
- * @description Classe Figure cria HTMLElement figure que pode ou não ter um HTMLElement img dentro, dando uma cor aleatória caso não haja um src válido para o img
+ * @description Classe Figure cria HTML figure que pode ou não ter um HTML img dentro, dando uma cor aleatória caso não haja um src válido para o img
+ * @author Vinicius Gonçalves Perillo --> https://github.com/ViniciusPerillo
  */
 export class Figure{
-    constructor(width = 0, height = 0, x = 0, y = 0, imgPath = '',){
+    /**
+     * @description Cria uma instância de Figure. Caso height seja omitido a altura será igual a largura. Caso o imgPath seja omitido ou o path seja inválido o HTML img seja escondido.
+     * @param {number} x px
+     * @param {number} y px
+     * @param {number} width px 
+     * @param {number} height px
+     * @param {string} imgPath 
+     */
+    constructor(x, y, width, height = width, imgPath = null,){
         this.imgPath = imgPath;
         this.figure = document.createElement('figure');
         document.body.appendChild(this.figure)
@@ -13,47 +22,29 @@ export class Figure{
         this.setPosition(x,y);
         this.setSize(width,height);
     }
-
+    
+    /**
+     * @description Define a distância do HTML figure em relação a borda esquerda e superior em uma mesma função.
+     * @param {number} x distância em relação a borda esquerda em px
+     * @param {number} y distância em relação a borda superior em px
+     */
     setPosition(x,y){
         this.setX = x;
         this.setY = y;
     }
 
-    setSize(width,height){
+    /**
+     * @description Define a Largura e Altura do HTML figure em uma mesma função. Caso height seja omitido a altura será igual a largura
+     * @param {number} width Largura em px
+     * @param {number} height Altura em px 
+     */
+    setSize(width,height = width){
         this.setWidth = width;
         this.setHeight = height;
     }
-//Getters-------------------------------------------------------------------------------------------------------------------------------
-    get getFigure(){
-        return this.figure;
-    }
-
-    get getImg(){
-        return this.img;
-    }
-
-    get getImgPath(){
-        return this.imgPath;
-    }
-
-    get getX(){
-        return this.figure.offsetLeft;
-    }
-
-    get getY(){
-        return this.figure.offsetTop;
-    }
-
-    get getWidth(){
-        return this.figure.offsetWidth;
-    }
-
-    get getHeight(){
-        return this.figure.offsetHeight;
-    }
-//Setters-------------------------------------------------------------------------------------------------------------------------------
     
     /**
+     * @description Define o src do HTML Element img
      * @param {string} path
      */
     set setImgPath(path){
@@ -65,21 +56,24 @@ export class Figure{
     }
 
     /**
-     * @param {number} x
+     * @description Define a distância do figure em relação a borda esquerda
+     * @param {number} x px
      */
     set setX(x){
         this.figure.style.left = `${x}px`;
     }
 
     /**
-     * @param {number} y
+     * @description Define a distância do figure em relação a borda superior
+     * @param {number} y px
      */
     set setY(y){
         this.figure.style.top = `${y}px`;
     }
 
     /**
-     * @param {number} width
+     * @description Define a largura do HTML figure e do HTML img
+     * @param {number} width px
      */
     set setWidth(width){
         this.img.style.width = `${width}px`;
@@ -87,11 +81,62 @@ export class Figure{
     }
     
     /**
-     * @param {number} height
+     * @description Define a altura do HTML figure e do HTML img
+     * @param {number} height px
      */
     set setHeight(height){
         this.img.style.height = `${height}px`;
         this.figure.style.height = `${height}px`;
+    }
+
+    /**
+     * @description Retorna o HTML figure
+     */
+    get getFigure(){
+        return this.figure;
+    }
+
+    /**
+     * @description Retorna o HTML img
+     */
+    get getImg(){
+        return this.img;
+    }
+
+    /**
+     * @description Retorna o src do HTML Element img
+     * @returns {string}
+     */
+    get getImgPath(){
+        return this.imgPath;
+    }
+
+    /**
+     * @description Retorna a distância do HTML figure em relação a borda esquerda da tela
+     */
+    get getX(){
+        return this.figure.offsetLeft;
+    }
+
+    /**
+     * @description Retorna a distância do HTML figure em relação a borda superior da tela
+     */
+    get getY(){
+        return this.figure.offsetTop;
+    }
+
+    /**
+     * @description Retorna a largura do HTML figure
+     */
+    get getWidth(){
+        return this.figure.offsetWidth;
+    }
+
+    /**
+     * @description Retorna a altura do HTML figure
+     */
+    get getHeight(){
+        return this.figure.offsetHeight;
     }
 }
 
